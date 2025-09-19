a4c3f56a233a449a87cd39cb7e2f10b568688e1f
IGalaxyI <mansur42915@gmail.com>
2025-09-20 00:36:35 +0500

    4

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"sort"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,30 +11,21 @@ type TreeNode struct {
 }
 
 func main() {
-
-}
-
-func zigzagLevelOrder(root *TreeNode) [][]int {
-	result := [][]int{}
-
-	var dfs func(*TreeNode, int)
-	dfs = func(node *TreeNode, depth int) {
-		if node == nil {
-			return
-		} else if len(result) == depth {
-			result = append(result, []int{})
-		}
-		if depth%2 == 0 {
-			result[depth] = append(result[depth], node.Val)
-		} else {
-			result[depth] = append([]int{node.Val}, result[depth]...)
-
-		}
-		dfs(node.Left, depth+1)
-		dfs(node.Right, depth+1)
+	nums1 := []int{1, 3}
+	nums2 := []int{2}
+	nums1 = append(nums1, nums2...)
+	sort.Ints(nums1)
+	s := float64(0)
+	n := float64(len(nums1))
+	if len(nums1)%2 == 1 {
+		return float64(nums1[n/2])
+	} else {
+		m1 := nums1[n/2-1]
+		m2 := nums1
+		return float64(r)
 	}
-
-	dfs(root, 0)
-
-	return result
 }
+
+// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+// 	print("момент")
+// }
