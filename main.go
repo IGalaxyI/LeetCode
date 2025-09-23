ca92a11da2a0b1444c2eafe952c4bef2c489a12e
IGalaxyI <mansur42915@gmail.com>
2025-09-23 16:34:52 +0500

    7

--- main.go
+++ main.go
@@ -1,54 +1,33 @@
 package main
 
 import (
-	"sort"
-	"strings"
+	"fmt"
 )
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 func main() {
-
+	fmt.Println(reverse(9646324351))
 }
 
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	nums1 = append(nums1, nums2...)
-	sort.Ints(nums1)
-	n := len(nums1)
-	if n%2 == 1 {
-		return float64(nums1[n/2])
-	} else {
-		m1 := nums1[n/2-1]
-		m2 := nums1[n/2]
-		return float64(m1+m2) / 2.0
+func reverse(x int) int {
+	const MaxInt32 = 1<<31 - 1
+	const MinInt32 = -1 << 31
+	save := 0
+	if x < 0 {
+		save = x
+		x = -x
 	}
-}
-
-func convert(s string, numRows int) string {
-	s := "PAYPALISHIRING"
-	numRows := 3
-
-	if numRows <= 1 {
+	s := 0
+	for x > 0 {
+		d := x % 10
+		if s > (MaxInt32-d)/10 {
+			return 0
+		}
+		s = s*10 + d
+		x /= 10
+	}
+	if save < 0 {
+		return -s
+	} else {
 		return s
 	}
-	rows := make([]strings.Builder, numRows)
-	row := 0
-	for _, i := range s {
-		rows[row].WriteRune(i)
-		if row == 0 {
-			dir = 1
-		} else if row == numRows-1 {
-			dir = -1
-		}
-		row += dir
-	}
-	var out strings.Builder
-	for i := 0; i < numRows; i++ {
-		out.WriteString(rows[i].String())
-	}
-	return out.String()
 }
