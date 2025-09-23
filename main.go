332269943c20ba01523a94191b6d19ba0f528572
IGalaxyI <mansur42915@gmail.com>
2025-09-23 14:50:06 +0500

    4

--- main.go
+++ main.go
@@ -11,21 +11,18 @@ type TreeNode struct {
 }
 
 func main() {
-	nums1 := []int{1, 3}
-	nums2 := []int{2}
+
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums1 = append(nums1, nums2...)
 	sort.Ints(nums1)
-	s := float64(0)
-	n := float64(len(nums1))
-	if len(nums1)%2 == 1 {
+	n := len(nums1)
+	if n%2 == 1 {
 		return float64(nums1[n/2])
 	} else {
 		m1 := nums1[n/2-1]
-		m2 := nums1
-		return float64(r)
+		m2 := nums1[n/2]
+		return float64(m1+m2) / 2.0
 	}
 }
-
-// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-// 	print("момент")
-// }
