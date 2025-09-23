f77398a21ccee96692ee2584ab1f02fc3e8b791f
IGalaxyI <mansur42915@gmail.com>
2025-09-23 15:02:51 +0500

    6

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sort"
+	"strings"
 )
 
 type TreeNode struct {
@@ -25,4 +26,29 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		m2 := nums1[n/2]
 		return float64(m1+m2) / 2.0
 	}
+}
+
+func convert(s string, numRows int) string {
+	s := "PAYPALISHIRING"
+	numRows := 3
+
+	if numRows <= 1 {
+		return s
+	}
+	rows := make([]strings.Builder, numRows)
+	row := 0
+	for _, i := range s {
+		rows[row].WriteRune(i)
+		if row == 0 {
+			dir = 1
+		} else if row == numRows-1 {
+			dir = -1
+		}
+		row += dir
+	}
+	var out strings.Builder
+	for i := 0; i < numRows; i++ {
+		out.WriteString(rows[i].String())
+	}
+	return out.String()
 }
