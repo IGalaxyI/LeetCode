1f7ab0e385a99ee52fd2e6c6ec1d68d43ff2e005
IGalaxyI <mansur42915@gmail.com>
2025-09-25 21:50:31 +0500

    15

--- main.go
+++ main.go
@@ -2,24 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println(minimumTotal([][]int{[[2],[3,4],[6,5,7],[4,1,8,3]]}))
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
 }
 
-func minimumTotal(triangle [][]int) int {
-    if len(triangle) == 0{
-		return triangle[0][0]
-	}
-	
-	dp := make([]int, len(triangle)+1)
-
-	for i := len(triangle)-1; i>=0;i--{
-		for j := 0; j<len(triangle[i]);j++{
-			dp[j] = triangle[i][j] + min(dp[j],dp[j+1])
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+	left := 0
+	right := 0
+	sum := 0
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		if nums[i] > 0 {
+			break
+		}
+		left = i + 1
+		right = len(nums) - 1
+		for left < right {
+			sum = nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else if sum < 0 {
+				left++
+			} else if sum > 0 {
+				right--
+			}
 		}
 	}
-	return dp[0]
+	return res
 
 }
