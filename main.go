1fcde17689fda721cea37c9697a5505ad340b06e
IGalaxyI <mansur42915@gmail.com>
2025-09-25 19:01:30 +0500

    12. 2 способа решения

--- main.go
+++ main.go
@@ -2,26 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
+	fmt.Println(intToRoman(3749))
+	fmt.Println(intToRoman2(3749))
 }
 
-func maxArea(height []int) int {
-	left := 0
-	right := len(height) - 1
-	maxArea := 0
-	for left < right {
-		area := (right - left) * min(height[left], height[right])
-		if area > maxArea {
-			maxArea = area
-		}
-		if height[left] < height[right] {
-			left++
-		} else if height[right] <= height[left] {
-			right--
+func intToRoman(num int) string {
+	thousands := []string{"", "M", "MM", "MMM"}
+	hundreds := []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	tens := []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	ones := []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+
+	th := num / 1000
+	h := (num % 1000) / 100
+	t := (num % 100) / 10
+	o := num % 10
+
+	result := thousands[th] + hundreds[h] + tens[t] + ones[o]
+	return result
+}
+
+func intToRoman2(num int) string {
+	type pair struct {
+		val int
+		sym string
+	}
+
+	pairs := []pair{{1000, "M"}, {900, "CM"},
+		{500, "D"}, {400, "CD"},
+		{100, "C"}, {90, "XC"},
+		{50, "L"}, {40, "XL"},
+		{10, "X"}, {9, "IX"},
+		{5, "V"}, {4, "IV"},
+		{1, "I"}}
+
+	var res strings.Builder
+	for _, p := range pairs {
+		for num >= p.val {
+			res.WriteString(p.sym)
+			num -= p.val
 		}
 	}
-	return maxArea
+	return res.String()
 }
