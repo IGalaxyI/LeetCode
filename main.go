dba55e35f928997d744231efb3ec12540823a14d
IGalaxyI <mansur42915@gmail.com>
2025-09-25 17:58:54 +0500

    8

--- main.go
+++ main.go
@@ -5,29 +5,42 @@ import (
 )
 
 func main() {
-	fmt.Println(reverse(9646324351))
+	fmt.Println(myAtoi("-042"))
 }
 
-func reverse(x int) int {
-	const MaxInt32 = 1<<31 - 1
+func myAtoi(s string) int {
 	const MinInt32 = -1 << 31
-	save := 0
-	if x < 0 {
-		save = x
-		x = -x
+	const MaxInt32 = 1<<31 - 1
+
+	i := 0
+	for i < len(s) && s[i] == ' ' {
+		i++
 	}
-	s := 0
-	for x > 0 {
-		d := x % 10
-		if s > (MaxInt32-d)/10 {
-			return 0
+
+	sign := 1
+
+	if i < len(s) {
+		if s[i] == '-' {
+			sign = -1
+			i++
+		} else if s[i] == '+' {
+			i++
 		}
-		s = s*10 + d
-		x /= 10
 	}
-	if save < 0 {
-		return -s
-	} else {
-		return s
+
+	res := 0
+	for i < len(s) && s[i] >= '0' && s[i] <= '9' {
+		digit := int(s[i] - '0')
+
+		if res > (MaxInt32-digit)/10 {
+			if sign == 1 {
+				return MaxInt32
+			}
+			return MinInt32
+		}
+		res = res*10 + digit
+		i++
 	}
+
+	return sign * res
 }
