6f417c6c8c0abd2498394e27b16470acb32d92e5
IGalaxyI <mansur42915@gmail.com>
2025-09-26 18:36:19 +0500

    611

--- main.go
+++ main.go
@@ -6,43 +6,23 @@ import (
 )
 
 func main() {
-	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Println(triangleNumber([]int{2, 2, 3, 4}))
 }
 
-func threeSum(nums []int) [][]int {
+func triangleNumber(nums []int) int {
 	sort.Ints(nums)
-	res := [][]int{}
-	left := 0
-	right := 0
-	sum := 0
-	for i := 0; i < len(nums)-2; i++ {
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
-		}
-		if nums[i] > 0 {
-			break
-		}
-		left = i + 1
-		right = len(nums) - 1
+	n := len(nums)
+	res := 0
+	for k := n - 1; k >= 2; k-- {
+		left, right := 0, k-1
 		for left < right {
-			sum = nums[i] + nums[left] + nums[right]
-			if sum == 0 {
-				res = append(res, []int{nums[i], nums[left], nums[right]})
-				left++
+			if nums[left]+nums[right] > nums[k] {
+				res += right - left
 				right--
-				for left < right && nums[left] == nums[left-1] {
-					left++
-				}
-				for left < right && nums[right] == nums[right+1] {
-					right--
-				}
-			} else if sum < 0 {
+			} else {
 				left++
-			} else if sum > 0 {
-				right--
 			}
 		}
 	}
 	return res
-
 }
