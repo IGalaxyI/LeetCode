ddbd7422d84ee5892d63d4478b7434b004a05d01
IGalaxyI <mansur42915@gmail.com>
2025-10-02 16:46:53 +0500

    19

--- main.go
+++ main.go
@@ -1,53 +1,54 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-func main() {
-	fmt.Println(fourSum([]int{1, 0, -1, 0, -2, 2}, 0))
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func fourSum(nums []int, target int) [][]int {
-	sort.Ints(nums)
-	result := [][]int{}
-	sum := 0
+func main() {
 
-	for i := 0; i < len(nums)-3; i++ {
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	slow := head
+	fast := head
+	//Двигаем fast на n вперед
+	for i := 0; i < n; i++ {
+		if fast != nil {
+			fast = fast.Next
 		}
-
-		for j := i + 1; j < len(nums)-2; j++ {
-			if j > i+1 && nums[j] == nums[j-1] {
-				continue
-			}
-
-			left := j + 1
-			right := len(nums) - 1
-
-			for left < right {
-				sum = nums[i] + nums[j] + nums[left] + nums[right]
-				if sum < target {
-					left++
-				} else if sum > target {
-					right--
-				} else {
-					result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})
-					left++
-					right--
-					for left < right && nums[left] == nums[left-1] {
-						left++
-					}
-					for left < right && nums[right] == nums[right+1] {
-						right--
-					}
-				}
-
-			}
-		}
-
 	}
-	return result
+	//Возвращяем если точка назначение это голова
+	if fast == nil {
+		return head.Next
+	}
+	//fast доходит до конца slow остается на значений удаления
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	//удаляем slow.Next
+	slow.Next = slow.Next.Next
+
+	return head
+}
+func removeNthFromEnd_2_0(head *ListNode, n int) *ListNode {
+	L := 0
+	//Вычисляем длину
+	for cur := head; cur != nil; cur = cur.Next {
+		L++
+	}
+	//Возвращяем если точка назначение это голова
+	if L == n {
+		return head.Next
+	}
+
+	var cur *ListNode = head
+	// Проходим до момента n
+	for i := 1; i < L-n; i++ {
+		cur = cur.Next
+	}
+	//Удаляем
+	cur.Next = cur.Next.Next
+	return head
 }
