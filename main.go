2907327f8a17f293e26d31068e18c35cbd3731b3
IGalaxyI <mansur42915@gmail.com>
2025-10-02 19:35:58 +0500

    22

--- main.go
+++ main.go
@@ -1,54 +1,32 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
-
+	fmt.Println(generateParenthesis(3))
 }
 
-func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	slow := head
-	fast := head
-	//Двигаем fast на n вперед
-	for i := 0; i < n; i++ {
-		if fast != nil {
-			fast = fast.Next
+func generateParenthesis(n int) []string {
+
+	res := []string{}
+
+	var dfs func(prefix string, open int, close int)
+	dfs = func(prefix string, open int, close int) {
+		if len(prefix) == 2*n {
+			res = append(res, prefix)
+		}
+		if open < n {
+			dfs(prefix+"(", open+1, close)
+		}
+		if close < open {
+			dfs(prefix+")", open, close+1)
 		}
 	}
-	//Возвращяем если точка назначение это голова
-	if fast == nil {
-		return head.Next
-	}
-	//fast доходит до конца slow остается на значений удаления
-	for fast != nil {
-		fast = fast.Next
-		slow = slow.Next
-	}
-	//удаляем slow.Next
-	slow.Next = slow.Next.Next
-
-	return head
-}
-func removeNthFromEnd_2_0(head *ListNode, n int) *ListNode {
-	L := 0
-	//Вычисляем длину
-	for cur := head; cur != nil; cur = cur.Next {
-		L++
-	}
-	//Возвращяем если точка назначение это голова
-	if L == n {
-		return head.Next
-	}
-
-	var cur *ListNode = head
-	// Проходим до момента n
-	for i := 1; i < L-n; i++ {
-		cur = cur.Next
-	}
-	//Удаляем
-	cur.Next = cur.Next.Next
-	return head
+	dfs("", 0, 0)
+	return res
 }
